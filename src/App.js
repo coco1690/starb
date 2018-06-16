5cf6c323fab670d07b097f386fc9a186d4bace60
caos <caos@gmail.com>
2018-06-16 16:55:47 -0500

    hackaos!

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Leftpanel from './components/leftpanel';
 import './App.css';
+ 
 import Centerpanel from "./components/centerpanel";
 import Carusel from "./components/carusel";
 import Perfil from "./components/perfil";
