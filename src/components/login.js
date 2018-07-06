87deacb0876264014fb7b8ce2e4b392c78143127
Jay Betancourt <oclean66@gmail.com>
2018-07-06 16:34:06 -0500

    login

--- src/components/login.js
+++ src/components/login.js
@@ -9,53 +9,80 @@ let canvas;
 class Login extends React.Component {
     constructor(props) {
         super(props)
-        this.state = { 
+        this.state = {
             login: false,
-            user:"",
-            pass:""
-            
+            user: "",
+            pass: ""
+
         }
 
         this.usuario = this.usuario.bind(this);
         this.password = this.password.bind(this);
-        this.ingresar= this.ingresar.bind(this);
+        this.ingresar = this.ingresar.bind(this);
     }
-            usuario(objuser) {
-                console.log(objuser.target.value)
-                this.setState({ user: objuser.target.value });
-            }
-            password(objpass) {
-                console.log(objpass.target.value)
-                this.setState({ pass: objpass.target.value });
-            }
-            ingresar(event) {
-                
-                // console.log(event.target);
-                // console.log(this.state.pass)
-                // console.log(this.state.pass)
-                let value = [this.state.pass, this.state.user];
-                // alert('A name was submitted: ' + this.state.value);
-                fetch('http://kingdeportes.com/gecko/api/login/m', {  mode: "cors", method: "POST", cache: "no-cache", body: JSON.stringify(value) }).then(results => {
-                    // console.log(results)    
-                return results;
-                }).then(data => {
-                    
-                    console.log(data.json())
-                });
-                event.preventDefault();
-            }
+    usuario(objuser) {
+        console.log(objuser.target.value)
+        this.setState({ user: objuser.target.value });
+    }
+    password(objpass) {
+        console.log(objpass.target.value)
+        this.setState({ pass: objpass.target.value });
+    }
+    ingresar(event) {
+
+        // console.log(event.target);
+        // console.log(this.state.pass)
+        // console.log(this.state.pass)
+        let value = { pass: this.state.pass, user: this.state.user };
+        // alert('A name was submitted: ' + this.state.value);
+        // fetch('http://localhost/gecko/api/login/m', 
+        // { method: "POST"                
+        // }).then(results => {
+        //     console.log(results)    
+        //     return results;
+        // }).then(data => {                    
+        //     console.log(data);
+        // });
+
+
+        const postData = (url = '', data = {}) => {
+            // Default options are marked with *
+            return fetch(url, {
+                method: "POST", // *GET, POST, PUT, DELETE, etc.
+                // mode: "no-cors", // no-cors, cors, *same-origin
+                // cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+                // credentials: "same-origin", // include, same-origin, *omit
+                headers: {
+                    // "Content-Type": "application/x-www-form-urlencoded",
+                    "Content-Type": "text/html",
+                },
+                // redirect: "follow", // manual, *follow, error
+                // referrer: "no-referrer", // no-referrer, *client
+                body: JSON.stringify({'data':data}), // body data type must match "Content-Type" header
+            })
+                .then(response => response.json()) // parses response to JSON
+                .catch(error => console.error(`Fetch Error =\n`, error));
+        };
+
+        postData('http://localhost/gecko/api/login/m', { username: this.state.user , pass:this.state.pass })
+            .then(data => console.log(data)) // JSON from `response.json()` call
+            .catch(error => console.error(error));
+
+
+        event.preventDefault();
+    }
 
 
     render() {
 
         if (!this.state.login) {
             canvas = <div className="headcont">
-                        <form onSubmit={this.ingresar}>
-                            <button className="btn" id="btnLogin">Ingresar </button>
-                            <input id="pass" placeholder="Contraseña" type="password" value={this.state.pass} onChange={this.password}/>
-                            <input id="email" placeholder="Usuario" type="text" value={this.state.user} onChange={this.usuario}/>
-                        </form>
-                    </div>
+                <form onSubmit={this.ingresar}>
+                    <button className="btn" id="btnLogin">Ingresar </button>
+                    <input id="pass" placeholder="Contraseña" type="password" value={this.state.pass} onChange={this.password} />
+                    <input id="email" placeholder="Usuario" type="text" value={this.state.user} onChange={this.usuario} />
+                </form>
+            </div>
 
         } else {
             return (
@@ -71,15 +98,15 @@ class Login extends React.Component {
 
                             </div>
                         </li>
-  {/* -------------------------------------------------------RELOJ_NAV-------------------------------------------------------------------- */}
-                        
+                        {/* -------------------------------------------------------RELOJ_NAV-------------------------------------------------------------------- */}
+
 
                         <li className="reloj">
                             <i className='ion-ios-alarm'></i>&nbsp;
-                            <i id="horas" className="horas"> : </i> : 
-                            <i id="minutos" className="minutos"> : </i>&nbsp; 
-                            <i id="segundos" className="segundos"> 0 </i> 
-                            <i id="ampm" className="ampm"> 0 </i> 
+                            <i id="horas" className="horas"> : </i> :
+                            <i id="minutos" className="minutos"> : </i>&nbsp;
+                            <i id="segundos" className="segundos"> 0 </i>
+                            <i id="ampm" className="ampm"> 0 </i>
                         </li>
                         <li>
                             <Link to="/">Historial</Link>
