1c149fe1eb30c29e01652835660d7f2d3d42184a
Jay Betancourt <oclean66@gmail.com>
2018-08-15 06:08:12 -0500

    perfil

--- src/components/perfil.js
+++ src/components/perfil.js
@@ -25,14 +25,14 @@ class Perfil extends Component {
         fetch('http://91.121.116.131/gecko/api/view/model/pccu/id/' + id, { cache: "no-cache" }).then(results => {
             return results.json();
         }).then(vertiket => {
+            console.log(vertiket)
             context.setState({
                 vertiket,
-                // entrada,
+                open: true 
+               
             })
-            context.setState({ open: true });
-
         });
-        // context.setState({ open: true })
+        
     }
 
     render() {
@@ -43,7 +43,7 @@ class Perfil extends Component {
             movimientos = usuario.Movimientos.map(obj => {
                 // console.log(i)
                 return (
-                    <tr>
+                    <tr key={"MM" + obj.ID}>
                         <td>{obj.ID}</td>
                         <td>{obj.Fecha}</td>
                         <td>{obj.GCUA_Id === 0 ? "Recarga" : (obj.GCUA_Id === 1 ? "Pago" : "Ajuste")}</td>
@@ -56,21 +56,21 @@ class Perfil extends Component {
 
         }
 
-        let tk = this.props.user.login ? this.props.user.userdata : { Tickets: [] };
+        let tq = this.props.user.login ? this.props.user.userdata : { Tickets: [] };
         let ultimostikets = {}
         if (this.props.user.login) {
-            ultimostikets = tk.Tickets.map(ob => {
+            ultimostikets = tq.Tickets.map(ob => {
                 // console.log(i)
                 return (
-                    <tr>
-                        <td>{ob.id}</td>
+                    <tr key={"Tk" + ob.Id}>
+                        <td>{ob.Id}</td>
                         <td>{ob.Fecha}</td>
-                        <td>{ob.Apuestas}</td>
+                        <td>{ob.nEventos}</td>
                         <td>{ob.Monto}</td>
                         <td>{ob.Ganancia}</td>
                         <td className={ob.Estado === 1 ? "abierto" : ob.Estado === 2 ? "ganador" : ob.Estado === 3 ? "perdedor" : ob.Estado === 5 ? "sin-efecto" : ob.Estado === 8 ? "ganador-cobrado" : ""}>{ob.Estado === 1 ? "Abierto" : ob.Estado === 2 ? "Ganador" : ob.Estado === 3 ? "Perdedor" : ob.Estado === 5 ? "Sin Efecto" : ob.Estado === 8 ? "Ganador-Cobrado" : ""}</td>
                         <td className='btn' >
-                            <img src="/img/icons/ticket.png" alt="" onClick={this.getdata.bind(this, ob.id)} /></td>
+                            <img src="/img/icons/ticket.png" alt="" onClick={this.getdata.bind(this, ob.Id)} /></td>
                     </tr>
 
 
@@ -80,9 +80,35 @@ class Perfil extends Component {
 
         }
 
-        let k = this.state.vertiket.cabezera
+        let d = this.state.vertiket.info
+        let o = this.state.vertiket.items ? this.state.vertiket.items : {};
+
+    let oo = Object.keys(o);
+    let tk = [];
+    if (this.props.user.login) {
+      tk = oo.map(ticket => {
+        let f = o[ticket]
+
+        return (
+
+          <div key={ticket} className="cliente-print">
+            <div>► Juego: {f.id} | {f.time}</div>
+            <div style={{ width: "100%" }}>  {f.liga} </div>
+            <div> </div>
+            <div>{f.name} </div>
+            <div>Apuesta: {f.option}
+              <div style={{ float: "right" }}>Cuota:
+              <span style={{ fontWeight: "bolder", fontSize: 14 }}>{f.odd}</span></div>
+            </div>
+          </div>
+
+        );
+
+      })
+    }
+
         let view = []
-        if (k) {
+        if (d) {
             // console.log(k.GCCA_Id)
             view = <div>
                 <Modal
@@ -90,26 +116,40 @@ class Perfil extends Component {
                     onHide={closeModal}
                     aria-labelledby="ModalHeader"
                 >
-                    <Modal.Header closeButton>
-                        <Modal.Title id='ModalHeader' style={{ color: "black" }}><div>{k.GCCA_Id}</div></Modal.Title>
-                        <div>
-                            {k.GCCA_Id}
-                        </div>
+                    <Modal.Header closeButton style={{ background: "rgb(5, 5, 5)" }}>
+                        <Modal.Title id='ModalHeader' style={{ color: "black" }}>Ticket Virtual</Modal.Title>                       
                     </Modal.Header>
-                    <Modal.Body>
-                        dfghjk
-                </Modal.Body>
-                    <Modal.Footer>
+                    <Modal.Body style={{ background: "rgb(5, 5, 5)" }} >
+                        <div>
+                            <div className="ticsk" >                               
 
-                        <Modal.Dismiss className='btn btn-default'>Cancel</Modal.Dismiss>
+                                <div className="cliente-print" style={{fontSize:14, paddingBottom:10}}>
+                                    <div>Agencia: {d.Agencia}</div>
+                                    <div>Fecha: {d.Fecha}</div>
+                                    <div>Ticket: {d.ID} </div>
+                                    <div>Usuario: {d.Usuario}</div>
+                                    <div>Estado: {d.Usuario}</div>
+                                    <div> Apuesta: <span style={{ fontSize: 14, fontWeight: "bolder" }}>${d.Monto}</span> </div>
+                                </div>
+                                {tk}
+                                
+                              
 
+                                <div id="ganancia-print">
+                                    <div className="ga-imprimir">COP {d.Ganancia}</div>
+                                </div>
+                               
+                            </div>
+                        </div>
 
-                        <button className='btn btn-primary' onClick={closeModal}>
-                            Save
-            </button>
+                    </Modal.Body>
+                    <Modal.Footer style={{ background: "rgb(5, 5, 5)" }}>
+                        <Modal.Dismiss className='btn confirm' 
+                        style={{ boxSizing: 'borderBox', width: '100%', height: 40, color: 'white', background: 'orange', fontSize: 14, border: 'hidden' }}>
+                        Cerrar</Modal.Dismiss>
                     </Modal.Footer>
                 </Modal>
-            </div>
+            </div >
             // console.log(k)
         }
 
@@ -130,20 +170,20 @@ class Perfil extends Component {
                                     <div id="form">
                                         <div>
                                             <div className="text"> Usuario</div>
-                                            <input disabled style={style.input} id="nombre" type="text" value={usuario.username} />
+                                            <input disabled style={style.input} id="nombre" type="text" defaultValue={usuario.username} />
                                         </div>
 
                                         <div>
                                             <div className="text"> Correo</div>
-                                            <input disabled style={style.input} id="nombre" type="text" value={usuario.email} />
+                                            <input disabled style={style.input} id="nombre" type="text" defaultValue={usuario.email} />
                                         </div>
                                         <div>
                                             <div className="text"> Telefono</div>
-                                            <input disabled style={style.input} id="nombre" type="text" value={usuario.Telefono} />
+                                            <input disabled style={style.input} id="nombre" type="text" defaultValue={usuario.Telefono} />
                                         </div>
                                         <div>
                                             <div className="text"> Zona </div>
-                                            <input disabled style={style.input} id="nombre" type="text" value={usuario.ZonaHora} />
+                                            <input disabled style={style.input} id="nombre" type="text" defaultValue={usuario.ZonaHora} />
                                         </div>
 
 
@@ -153,16 +193,16 @@ class Perfil extends Component {
 
                                         <div>
                                             <div className="text"> Nombre Completo</div>
-                                            <input disabled style={style.input} id="nombre" type="text" value={usuario.fullname} />
+                                            <input disabled style={style.input} id="nombre" type="text" defaultValue={usuario.fullname} />
                                         </div>
 
                                         <div>
                                             <div className="text"> Direccion</div>
-                                            <input disabled style={style.input} id="nombre" type="text" value={usuario.Direccion} />
+                                            <input disabled style={style.input} id="nombre" type="text" defaultValue={usuario.Direccion} />
                                         </div>
                                         <div>
                                             <div className="text"> Pais</div>
-                                            <input disabled style={style.input} id="nombre" type="text" value={usuario.Pais} />
+                                            <input disabled style={style.input} id="nombre" type="text" defaultValue={usuario.Pais} />
                                         </div>
                                     </div>
                                 </div>
